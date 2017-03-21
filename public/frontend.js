088b5710aa13d8b1770a10b819b045eecea2f63f
brennnnan <brennan.mackay@sbcglobal.net>
2017-03-21 10:24:56 -0700

    added frontend with user instructions

--- public/frontend.js
+++ public/frontend.js
@@ -1,10 +1,10 @@
 $(function () {
 	"use strict";
 
-	var content = $('#content');
     var input = $('#input');
-    var status = $('#status');
-
+	var midiInput;
+	var channelCount = -1;
+	
     var notes = []
     notes.push(new Audio('audio/2aa.wav'));
     notes.push(new Audio('audio/2b.wav'));
@@ -19,76 +19,195 @@ $(function () {
     notes.push(new Audio('audio/2gg.wav'));
     notes.push(new Audio('audio/3a.wav'));
 	
+	
+	
 
 	var socket = io();
 	if(socket) {
-		status.text('Choose name:');
-		//input.removeAttr('disabled');
+		// Display options
 	}
 
 	var myName = false;
 
-
+	var masterButton = document.getElementById('master_button');
+	masterButton.addEventListener('click', function(){roleSelected(0)}, true);
+	
+	var servantButton = document.getElementById('servant_button');
+	servantButton.addEventListener('click', function(){roleSelected(1)}, true);
 
 	socket.on('note', function(noteInfo) {
 		console.log('received');
 		if(noteInfo.note >= 0 && noteInfo.note < 128) {
 			console.log(myName+" received "+noteInfo.note);
             //output.playNote(noteInfo.note, "all", {time: WebMidi.time + 3000});
-            notes[(noteInfo.note+2)%12].play();
-            setTimeout(function(){output.stopNote(noteInfo.note)}, 3500);
+            //notes[(noteInfo.note+2)%12].play();
+            //setTimeout(function(){output.stopNote(noteInfo.note)}, 3500);
         }	
 	})
+	
+	socket.on('receipt', function(receiptInfo) {
+		console.log(receiptInfo.group)
+		if(receiptInfo.group == -1) loadAdminInfoInterface();
+		else loadServantInfoInterface();
+	})
+	
+	
+	function roleSelected(role) {
+		console.log(role+' stuf')
+		if(role===0) {
+			var obj = {
+				role: 'admin'
+			}
+			socket.emit('name', obj);
+		} else if(role==1) {
+			var obj = {
+				role: 'servant'
+			}
+			socket.emit('name', obj);
+		}
+	}
+	
+	function enableMidi(role) {
+		WebMidi.enable(function (err) {
+  
+    		if (err) {
+      			console.log("WebMidi could not be enabled.", err);
+    		} else {
+      			console.log("WebMidi enabled!");
+      			console.log(WebMidi.inputs);
+      			console.log(WebMidi.outputs);
+				if(role=='admin') {
+					var midiInputs = document.getElementById('midi_input_list')
+					var options = [];
+		  			for(var d=0; d<WebMidi.inputs.length; d++) {
+		  				options.push(WebMidi.inputs[d].name);
+					}
+					if(options.length>0) midiInput = WebMidi.getInputByName(options[0])
+					for (var i = 0; i < options.length; i++) {
+		    			var option = document.createElement('option');
+		    			option.value = options[i];
+		    			option.innerHTML = options[i];
+		    			midiInputs.appendChild(option);
+		  			}
 
-	input.keydown(function(e) {
+					midiInputs.onchange = function() {
+						midiInput = WebMidi.getInputByName(this.value);
+  					}
+					var channels = document.getElementById('channel_list')
+					channels.onchange = function() {
+						channelCount = this.value;
+						console.log(channelCount)
+					}
+					var submitButton = document.getElementById('admin_submit_button')
+					submitButton.addEventListener("click", loadAdminDisplay)
+		  		}
+				
+				if(role=='servant') {
+					var midiOutputs = document.getElementById('midi_output_list')
+					var options = [];
+		  			for(var d=0; d<WebMidi.outputs.length; d++) {
+		  				options.push(WebMidi.outputs[d].name);
+					}
+					for (var i = 0; i < options.length; i++) {
+		    			var option = document.createElement('option');
+		    			option.value = options[i];
+		    			option.innerHTML = options[i];
+		    			midiOutputs.appendChild(option);
+		  			}
 
-        if (e.keyCode === 13) {
-            var msg = $(this).val();
-            if (!msg) {
-                return;
-            }
-            if (myName === false) {
-            	var obj = {
-            		chosenName: msg,
-            		channels: channelCount
-            	}
-                socket.emit('name',obj)
-                myName = msg;
-                if (myName == 'admin') {
-                    status.text('hi admin!');
-                    $(this).val('');
-                } else {
-                	status.text('Hi '+myName+'!')
-                	$(this).val('');
-                	//input.attr('disabled', 'disabled');
-                }
-                return;
-            }
+					midiOutputs.onchange = function() {
+						midiOutput = WebMidi.getOutputByName(this.value);
+  					}
+					var submitButton = document.getElementById('servant_submit_button')
+					submitButton.addEventListener("click", loadServantDisplay)
+				}
+				
+			}
+    	});
+	}
+	
+	function loadAdminInfoInterface() {
+		enableMidi('admin');
+		document.getElementById("boxes").style.display = "none";
+		document.getElementById("admin_information").style.display = "block"
+	}
+	
+	function loadServantInfoInterface() {
+		enableMidi('servant');
+		document.getElementById("boxes").style.display = "none";
+		document.getElementById("servant_information").style.display = "block"
+	}
+	
+	function loadAdminDisplay() {
+		var obj = {
+			channelCount: channelCount
+		}
+		socket.emit("adminInfo", obj);
+		
+		
+		document.getElementById("admin_information").style.display = "none"
+		var note_sent_display = document.createElement('div');
+		note_sent_display.setAttribute('class', 'note_light_up_container')
+		var mainContainer = document.getElementById("main_container");
+		mainContainer.appendChild(note_sent_display);
+		
+		//Create visual display boxes for each channel in channelCount
+		var boxes = []
+		for(var i=0; i<channelCount; i++) {
+			var div = document.createElement('div')
+			div.setAttribute('class', 'channel_display_box');
+			div.style.width = 80/(channelCount%8)+"%";
+			div.style.marginRight = 18/(channelCount%8)+"%";
+			boxes.push(div)
+			note_sent_display.appendChild(div);
+		}
+		
+		//bind display boxes to channel listeners
+		for(var g=1; g<=channelCount; g++){
+			var myCount = g;
+			(function (_myCount) {
+				midiInput.addListener('noteon', _myCount, function (e) {
+				//console.log("Received 'noteon' message (" + e.note.name + e.note.octave + "1). ");
+					boxes[_myCount-1].style.background = "#E066FF";
+					var obj = {
+						note: e.note,
+						channel: _myCount
+					}
+					socket.emit('noteon',obj);
+				});
+				midiInput.addListener('noteoff', _myCount, function (e) {
+				//console.log("Received 'noteoff' message (" + e.note.name + e.note.octave + "1). ");
+					boxes[_myCount-1].style.background = "#FFF";
+					var obj = {
+						note: e.note,
+						channel: _myCount
+					}
+					socket.emit('noteoff',obj);
+				});
+			})(myCount);
+			
+		}
+	}
+	
+	function loadServantDisplay() {
+		document.getElementById("servant_information").style.display = "none"
+		var note_sent_display = document.createElement('div');
+		note_sent_display.setAttribute('class', 'note_light_up_container')
+		var mainContainer = document.getElementById("main_container");
+		mainContainer.appendChild(note_sent_display);
+		
+		var div = document.createElement('div')
+		div.setAttribute('class', 'channel_display_box');
+		div.style.width = "40%"
+		note_sent_display.appendChild(div);
+		
+		midiInput.addListener('noteon', "all", function (e) {
+			div.style.background = "#E066FF";
+		});
+		
+		midiOuput.addListener('noteoff', "all", function (e) {
+			div.style.background = "#FFF";
+		});	
+	}
 
-            var info = msg.split(" ");
-            var obj = {
-            	note: info[0]
-            }
-
-            socket.emit('note', obj)
-            console.log('Note '+info[0]+' sent.')
-            $(this).val('');
-            //input.attr('disabled', 'disabled');
-        }
-		/*		
-        if(e.keyCode === 32) {
-        	var obj = {
-        		note: 46,
-        		channel: 1
-        	}
-
-        	var obj1 = {
-        		note: 54,
-        		channel: 2
-        	}
-        	setInterval(() => socket.emit('note', obj), 500);
-        	setInterval(() => socket.emit('note', obj1), 1000);
-        }
-        */
-    });
 });
--- server.js
+++ server.js
@@ -1,68 +1,70 @@
 'use strict';
-
 /**
  * Global variables
  */
 // latest 100 messages
-var history = [ ];
+var history = [];
 // list of currently connected clients (users)
-var clients = [ ];
-
-
+var clients = [];
 //Setup
 var http = require('http');
 var path = require('path');
-var express = require('express'),
-    app = module.exports.app = express();
+var express = require('express')
+	, app = module.exports.app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
-
 var server = http.createServer(app);
 var serverChannelCount = 1;
-var io = require('socket.io').listen(server);  //pass a http.Server instance
-server.listen(PORT);  //listen on port 
-console.log('Server listening on port '+PORT);
-
+var io = require('socket.io').listen(server); //pass a http.Server instance
+server.listen(PORT); //listen on port 
+console.log('Server listening on port ' + PORT);
 
 
 io.on('connection', (socket) => {
-	var index = clients.push([socket,clients.length,0]) - 1;
-	var userName = false;
-
+	var index = clients.push([socket, clients.length, 0]) - 1;
+	var userRole = false;
 	console.log((new Date()) + ' Connection accepted.');
-
-	socket.on('name',(name) => {
-		userName = htmlEntities(name.chosenName);
-
-		if(userName == 'admin') {
-			console.log((new Date()) + ' Admin is present with '+name.channels+' channels.');
-			serverChannelCount = name.channels;
-			clients[index][2]==1;
-		}
-        else {
-			console.log((new Date()) + ' User is known as: ' + userName+ '.');
-		}
+	
+  	socket.on('name', (name) => {
+			userRole = htmlEntities(name.role);
+			if (userRole == 'admin') {
+				console.log((new Date()) + ' Admin is present with ' + name.channels + ' channels.');
+				clients[index][2] == 1;
+				var obj = {
+					group: -1
+				}
+				socket.emit('receipt', obj);
+			}
+		
+			else {
+				console.log((new Date()) + ' User is known as: ' + userRole + '.');
+				var obj = {
+					group: 2
+				}
+				socket.emit('receipt', obj);
+				//return group membership id over socket
+			}
 	})
-
+	
+	socket.on('adminInfo', (adminInfo) => {
+		serverChannelCount = adminInfo.channelCount;
+	})
+	
 	// only when admin sends note
-	socket.on('note',(noteinfo) => {
-		console.log((new Date()) + ' Received Message from '+ userName + ': '+noteinfo.note);
-
-		/* we want to keep history of all sent messages
+	socket.on('note', (noteinfo) => {
+		console.log((new Date()) + ' Received Message from ' + userName + ': ' + noteinfo.note);
+		// we want to keep history of all sent messages
 		var obj = {
 			time: (new Date()).getTime(),
-			note: noteDetails[0],
-			noteLength: noteDetails[1],
+			note: noteinfo.note,
 			author: userName,
 		};
                 
         history.push(obj);
 		history = history.slice(-100);
-		*/
 
 		// broadcast message to all connected clients
 		socket.broadcast.emit('note', noteinfo);
-
 		/* send to individual channels based on ids
 		for(var i=0; i<clients.length; i++) {
 			if(clients[i][1]%serverChannelCount==noteinfo.channel-1 && clients[i][2]==0) {
@@ -70,12 +72,11 @@ io.on('connection', (socket) => {
 			} 
 		} */
 	})
-
-  	socket.on('disconnect', () => console.log('Client disconnected'));
+	
+	
+	socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-
 function htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;')
-                      .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+	return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
